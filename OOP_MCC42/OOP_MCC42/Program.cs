da11585fb214f38a5348fe9a029a8655bb0ceacc
rzkyamla <63730714+rzkyamla@users.noreply.github.com>
2021-01-08 11:03:38 +0700

    First Commit

---
+++ OOP_MCC42/OOP_MCC42/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_MCC42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
